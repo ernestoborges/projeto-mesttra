330442c71a137a82528e91181efe1e07400c99c5
Ernesto Neto <ernestoborges1995@gmail.com>
2023-08-02 12:00:13 -0300

    fix: Fix sequelize webpack to fit vercel

--- server/src/db/models/index.ts
+++ server/src/db/models/index.ts
@@ -3,17 +3,27 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-// const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db: any = {};
 
+import mysql2 from "mysql2";
+
 let sequelize: any;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(
+    process.env[config.use_env_variable], 
+    config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database, 
+    config.username, 
+    config.password, 
+    {
+      ...config,
+      dialectModule: mysql2,
+    });
 }
 
 fs
