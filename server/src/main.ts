512b28814f2db7b5f705847bf401f15675fc1b65
Ernesto Neto <ernestoborges1995@gmail.com>
2023-08-04 10:13:03 -0300

    config: Configuration to Railwaydeploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,15 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import db from "./db/models";
 // import { usersSeedList } from "./db/seeders/users";
-import bcrypt from "bcrypt";
 import router from "./routes";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = 3000;
+
+const PORT = Number(process.env.PORT) || 3000;
 
 
 app.get("/", (req: Request, res: Response) => {
@@ -19,7 +21,7 @@ app.get("/", (req: Request, res: Response) => {
 app.use('/api', router);
 
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0",() => {
     console.log(`Servidor rodando na porta: ${PORT}`);
   });
 })
